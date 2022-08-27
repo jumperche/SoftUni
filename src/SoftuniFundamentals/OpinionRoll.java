9d471c293b4e22319b0483bdfb68c677eea5d35b
jumperche <jumperche@gmail.com>
2022-08-27 13:34:29 +0200

    new problems

--- src/SoftuniFundamentals/OpinionRoll.java
+++ src/SoftuniFundamentals/OpinionRoll.java
@@ -1,27 +1,29 @@
 package SoftuniFundamentals;
-
+//Using the Person class, write a program that reads from the console N lines of personal
+// information and then prints all people whose age is more than 30 years.
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class OpinionRoll {
     public static void main ( String[] args ) {
-      List<Person>persons=new ArrayList<> () ;
+        List< Person > persons = new ArrayList<> ( );
         Scanner scanner = new Scanner ( System.in );
         int n = Integer.parseInt ( scanner.nextLine ( ) );
         for ( int i = 0 ; i < n ; i++ ) {
-            String input=scanner.nextLine ();
-            String name=input.split ( " " )[0];
-            int age=Integer.parseInt ( input.split ( " " )[1]);
-            if(age>30) {
+            String input = scanner.nextLine ( );
+            String name = input.split ( " " )[ 0 ];
+            int age = Integer.parseInt ( input.split ( " " )[ 1 ] );
+            if ( age > 30 ) {
                 Person per = new Person ( name , age );
-                System.out.println (per.printPerson () );
+                System.out.println ( per.printPerson ( ) );
             }
 
         }
 
     }
-    public static class Person{
+
+    public static class Person {
         String name;
         int age;
 
@@ -45,11 +47,12 @@ public class OpinionRoll {
         public void setAge ( int age ) {
             this.age = age;
         }
-        public String printPerson(){
 
-                    return this.getName ()+" - "+this.getAge () ;
+        public String printPerson () {
 
-            }
+            return this.getName ( ) + " - " + this.getAge ( );
+
         }
     }
+}
 
