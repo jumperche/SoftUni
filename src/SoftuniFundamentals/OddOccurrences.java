e9b43ef9a2e63637099e65c7132081f8dc585085
jumperche <jumperche@gmail.com>
2022-09-02 14:29:59 +0200

    new problems

--- src/SoftuniFundamentals/OddOccurrences.java
+++ src/SoftuniFundamentals/OddOccurrences.java
@@ -1,5 +1,7 @@
 package SoftuniFundamentals;
-
+//Write a program that extracts from a given sequence of words all elements that are present in it an odd number of times (case-insensitive).
+//•	Words are given in a single line, space-separated.
+//•	Print the result elements in lowercase in their order of appearance.
 import java.util.*;
 import java.util.stream.Collectors;
 
