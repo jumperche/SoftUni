38112875ffb4e65a06c22ed37324ba66748e0cf2
jumperche <jumperche@gmail.com>
2022-09-04 17:12:02 +0200

    new problems

--- src/SoftuniFundamentals/CompanyUsers.java
+++ src/SoftuniFundamentals/CompanyUsers.java
@@ -1,5 +1,15 @@
 package SoftuniFundamentals;
-
+//Write a program which keeps the information about companies and their employees.
+//You will be receiving company names and an employees' id until you receive the command "End" command. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
+//Print the company name and each employee's id in the following format:
+//"{company_name}
+//-- {id1}
+//-- {id2}
+//…
+//-- {idN}"
+//Input / Constraints
+//•	Until you receive "End", the input come in the format: "{companyName} -> {employeeId}".
+//•	The input always will be valid.
 import java.util.*;
 
 public class CompanyUsers {
