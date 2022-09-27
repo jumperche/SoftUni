8eb3e5ea9d20d6b45c65354445aced2af099bac6
jumperche <jumperche@gmail.com>
2022-09-27 08:02:53 +0200

    new problems

--- src/SoftuniFundamentals/FromLeftToTheRight.java
+++ src/SoftuniFundamentals/FromLeftToTheRight.java
@@ -1,39 +1,41 @@
 package SoftuniFundamentals;
 //You will receive a number that represents how many lines we will get as input. On the next N lines, you will receive a string with 2 numbers separated by a single space. You need to compare them. If the left number is greater than the right number, you need to print the sum of all digits in the left number, otherwise, print the sum of all digits in the right number
-import java.util.*;
+
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class FromLeftToTheRight {
     public static void main ( String[] args ) {
         Scanner scanner = new Scanner ( System.in );
         int n = Integer.parseInt ( scanner.nextLine ( ) );
         int sum = 0;
-
-        for ( int i = 0 ; i < n; i++ ) {
-
-
+        for ( int i = 0 ; i < n ; i++ ) {
             String[] input = scanner.nextLine ( ).split ( " " );
-            double x = Double.parseDouble ( input[ 0 ] );
-            double y = Double.parseDouble ( input[ 1 ] );
-
-            if ( x >= y ) {
-                while ( x != 0 ) {
-                    int a = (int)x % 10;
-                    sum = sum + a;
-                    x = x / 10;
+            BigInteger x = new BigInteger ( input[ 0 ] );
+            BigInteger y = new BigInteger ( input[ 1 ] );
+            BigInteger zero = new BigInteger ( String.valueOf ( 0 ) );
+            if ( x.max ( y ) == x ) {
+                while ( !x.equals ( zero ) ) {
+                    BigInteger modulo = new BigInteger ( String.valueOf ( 10 ) );
+                    BigInteger a = x.mod ( modulo );
+                    sum = sum + a.intValue ( );
+                    x = x.divide ( modulo );
                 }
+
             } else {
-                while ( y != 0 ) {
-                    int a =(int) y % 10;
-                    sum = sum + a;
-                    y = y / 10;
+                while ( !y.equals ( zero ) ) {
+                    BigInteger modulo = new BigInteger ( String.valueOf ( 10 ) );
+                    BigInteger a = y.mod ( modulo );
+                    sum = sum + a.intValue ( );
+                    y = y.divide ( modulo );
                 }
             }
-            System.out.println (sum );
-            sum=0;
-            //input = scanner.nextLine ( ).split ( " " );
+            System.out.println ( sum );
+            sum = 0;
         }
     }
+}
 
 
-    }
+
 
