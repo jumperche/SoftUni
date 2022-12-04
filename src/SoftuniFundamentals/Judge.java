980419208c79acc3ae293cb7330594dbac8ab619
jumperche <jumperche@gmail.com>
2022-12-04 16:14:40 +0100

    new problems

--- src/SoftuniFundamentals/Judge.java
+++ src/SoftuniFundamentals/Judge.java
@@ -2,7 +2,27 @@ package SoftuniFundamentals;
 
 import java.util.*;
 import java.util.stream.Collectors;
+//You know the Judge system, right?! Your job is to create a program similar to the Judge system.
+//You will receive several input lines in the following format:
+//"{username} -> {contest} -> {points}"
+//The constestName and username are strings. The given points will be an integer number. You need to keep track of every contest and individual statistics of every user. You should check if such a contest already exists, and if not, add it, otherwise, check if the current user is participating in the contest. If he is participating, take the higher score, otherwise, just add it.
+//Also, you need to keep individual statistics for each user - the total points of all contests.
+//You should end your program when you receive the command "no more time". At that point, you should print each contest in order of input. For each contest, print the participants ordered by points in descending order and then by name in ascending order. After that, you should print individual statistics for every participant ordered by total points in descending order and then by alphabetical order.
+//Input / Constraints
+//•	The input comes in the form of commands in the format specified above.
+//•	Username and contest name always will be one word.
+//•	Points will be an integer in the range [0, 1000].
+//•	There will be no invalid input lines.
+//•	If all sorting criteria fail, the order should be by order of input.
+//•	The input ends when you receive the command "no more time".
 
+//Peter -> Algo -> 400
+//George -> Algo -> 300
+//George -> Algo -> 900
+//Sam -> Algo -> 200
+//Peter -> DS -> 150
+//Maria -> DS -> 600
+//no more time
 public class Judge {
     public static List< Student > list = new ArrayList<> ( );
     public static List< String > listContest = new ArrayList<> ( );
